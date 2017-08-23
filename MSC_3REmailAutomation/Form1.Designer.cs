af8461c2187bb7722e4933d7829ad7b4383d23b6
testing <vsumanthgeorge@gmail.com>
2017-08-23 14:50:57 +0530

    Commit test

--- MSC_3REmailAutomation/Form1.Designer.cs
+++ MSC_3REmailAutomation/Form1.Designer.cs
@@ -68,9 +68,10 @@
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.label2.Location = new System.Drawing.Point(568, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(179, 20);
+            this.label2.Size = new System.Drawing.Size(228, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "3R Reminder Email App";
+            this.label2.Text = "3R Reminder Email Application";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
